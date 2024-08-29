23145454810f2a0072de4f0171abb21e8efb2fc9
EvolutionX <huntersmith.has@gmail.com>
2024-08-28 20:38:53 -0500

    Update

--- src/main/java/net/EvolutionX/TutorialMod/TutorialMod.java
+++ src/main/java/net/EvolutionX/TutorialMod/TutorialMod.java
@@ -1,6 +1,9 @@
 package net.EvolutionX.TutorialMod;
 
 import com.mojang.logging.LogUtils;
+import net.EvolutionX.TutorialMod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -35,6 +38,8 @@ public class TutorialMod
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -50,7 +55,11 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.ALEXANDRITE);
+            event.accept(ModItems.RAW_ALEXANDRITE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
