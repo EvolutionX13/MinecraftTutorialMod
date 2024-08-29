455396499fd131e576084f2ce7afdfd5b5b9c822
EvolutionX <huntersmith.has@gmail.com>
2024-08-28 23:08:53 -0500

    Update (Block texture not loading)

--- src/main/java/net/EvolutionX/TutorialMod/TutorialMod.java
+++ src/main/java/net/EvolutionX/TutorialMod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.EvolutionX.TutorialMod;
 
 import com.mojang.logging.LogUtils;
+import net.EvolutionX.TutorialMod.block.ModBlocks;
 import net.EvolutionX.TutorialMod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -39,6 +40,7 @@ public class TutorialMod
         MinecraftForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
@@ -59,6 +61,11 @@ public class TutorialMod
         {
             event.accept(ModItems.ALEXANDRITE);
             event.accept(ModItems.RAW_ALEXANDRITE);
+        }
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
+        {
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
         }
     }
 
